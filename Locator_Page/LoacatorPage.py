461e9933a2184afa10a31f54b9dbf17a81e4b560
Psalmbrovic <abatansamuel@ymail.com>
2024-11-12 07:44:59 +0100

    inprogress

--- Locator_Page/LoacatorPage.py
+++ Locator_Page/LoacatorPage.py
@@ -7,9 +7,20 @@ class SignIn:
     LOGIN_BUTTON = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div[3]/form/div[4]/button/div')
     CREATE_ACCOUNT = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div[3]/form/div[5]/a')
     
-# class GetStarted:
-#     EMAIL_ADDRESS = (By.ID, "")
-#     PASSWORD = (By.ID, "")
+
+class CreateAnAccount:
+    SIGNUP = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[2]/div[3]/form/div[5]/a')
+    BUSINESS_NAME = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[3]/form/div[1]/div/input')
+    FULL_NAME = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[3]/form/div[2]/div/input')
+    EMAIL_ADDRESS = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[3]/form/div[3]/div/input')
+    PHONE_NUMBER = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[3]/form/div[4]/div/input')
+    PASSWORD = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[3]/form/div[5]/div/input')
+    TERMS_CONDITIONS = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[3]/form/div[6]/a')
+    CREATE_ACCOUNT_1 = (By.XPATH,'//*[@id="app"]/div[1]/div[2]/div[3]/form/div[7]/button/div')
+
+class GetStarted:
+     GETSTARTED_BUTTON = (By.Xpath, '//*[@id="app"]/div[1]/div[1]/div[1]/div[1]')
+     EDIT_BUSINESS_PROFILE = (By.XPATH, '//*[@id="app"]/div[1]/div[2]/div[3]/div[3]/div[1]/div/div[2]/button')
 
 # class LoginLocator:
 #     EMAIL_ADDRESS = (By.ID, "")
